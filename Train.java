91696a5d97ee59165b474b7bfc4bc7e0c2a89fbd
Wicak <wicaksono.hm@gmail.com>
2024-12-20 18:16:12 +0800

    Stack

--- Train.java
+++ Train.java
@@ -4,33 +4,30 @@ class Train {
     String departureStation;
     String schedule;
     int capacity;
-    Passenger headPassenger;
+    Passenger topPassenger;
 
     Train(String name, String departureStation, String schedule, int capacity) {
         this.name = name;
         this.departureStation = departureStation;
         this.schedule = schedule;
         this.capacity = capacity;
-        this.headPassenger = null;
+        this.topPassenger = null;
     }
 
     void addPassenger(String passengerName) {
         System.out.println("Berhasil menambahkan " + passengerName + " ke dalam kereta " + name);
         Passenger newPassenger = new Passenger(passengerName);
-        if (headPassenger == null) {
-            headPassenger = newPassenger;
+        if (topPassenger == null) {
+            topPassenger = newPassenger;
         } else {
-            Passenger temp = headPassenger;
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = newPassenger;
+            newPassenger.next = topPassenger;
+            topPassenger = newPassenger;
         }
         capacity--;
     }
 
     void displayPassengers() {
-        Passenger temp = headPassenger;
+        Passenger temp = topPassenger;
         System.out.println("Penumpang Kereta " + name + ":");
         if (temp == null) {
             System.out.println("Tidak ada penumpang.");
